f87f4cc5292ea01d9ccbf24b36f82c884bec4d28
lilkr <maintiendrai@gmail.com>
2015-10-20 11:29:29 +0800

    add resources

--- JESwitchSoundOnWatchOS2/ViewController.m
+++ JESwitchSoundOnWatchOS2/ViewController.m
@@ -8,7 +8,17 @@
 
 #import "ViewController.h"
 
+#import "MMWormhole.h"
+#import "MMWormholeSession.h"
+
 @interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *numberLabel;
+@property (nonatomic, weak) IBOutlet UISegmentedControl *segmentedControl;
+
+@property (nonatomic, strong) MMWormhole *traditionalWormhole;
+@property (nonatomic, strong) MMWormhole *watchConnectivityWormhole;
+@property (nonatomic, strong) MMWormholeSession *watchConnectivityListeningWormhole;
 
 @end
 
@@ -16,12 +26,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Initialize the wormhole
+    self.traditionalWormhole = [[MMWormhole alloc] initWithApplicationGroupIdentifier:@"group.com.lilkr.lilkrtest"
+                                                                    optionalDirectory:@"wormhole"];
+    
+    // Initialize the MMWormholeSession listening wormhole.
+    // You are required to do this before creating a Wormhole with the Session Transiting Type, as we are below.
+    self.watchConnectivityListeningWormhole = [MMWormholeSession sharedListeningSession];
+    
+    // Initialize the wormhole using the WatchConnectivity framework's application context transiting type
+    self.watchConnectivityWormhole = [[MMWormhole alloc] initWithApplicationGroupIdentifier:@"group.com.lilkr.lilkrtest"
+                                                                          optionalDirectory:@"wormhole"
+                                                                             transitingType:MMWormholeTransitingTypeSessionContext];
+    
+    // Become a listener for changes to the wormhole for the button message
+    [self.traditionalWormhole listenForMessageWithIdentifier:@"button" listener:^(id messageObject) {
+        // The number is identified with the buttonNumber key in the message object
+        NSNumber *number = [messageObject valueForKey:@"buttonNumber"];
+        self.numberLabel.text = [number stringValue];
+    }];
+    
+    // Become a listener for changes to the wormhole for the button message
+    [self.watchConnectivityListeningWormhole listenForMessageWithIdentifier:@"button" listener:^(id messageObject) {
+        // The number is identified with the buttonNumber key in the message object
+        NSNumber *number = [messageObject valueForKey:@"buttonNumber"];
+        self.numberLabel.text = [number stringValue];
+    }];
+    
+    // Make sure we are activating the listening wormhole so that it will receive new messages from
+    // the WatchConnectivity framework.
+    [self.watchConnectivityListeningWormhole activateSessionListening];
+    
+    [self segmentedControlValueDidChange:self.segmentedControl];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    // Obtain an initial message from the wormhole
+    id messageObject = [self.traditionalWormhole messageWithIdentifier:@"button"];
+    NSNumber *number = [messageObject valueForKey:@"buttonNumber"];
+    
+    self.numberLabel.text = [number stringValue];
+    
+    // Obtain an initial message from the wormhole
+    messageObject = [self.watchConnectivityWormhole messageWithIdentifier:@"button"];
+    number = [messageObject valueForKey:@"buttonNumber"];
+    
+    self.numberLabel.text = [number stringValue];
 }
+
+- (IBAction)segmentedControlValueDidChange:(UISegmentedControl *)segmentedControl {
+    NSString *title = [segmentedControl titleForSegmentAtIndex:segmentedControl.selectedSegmentIndex];
+    
+    // Pass a message for the selection identifier. The message itself is a NSCoding compliant object
+    // with a single value and key called selectionString.
+    //    if (segmentedControl.selectedSegmentIndex == 1) {
+    //        title = @"http://7vzmkg.com1.z0.glb.clouddn.com/wave/1AAD605ED1A5EDEB.mp3";
+    title = @"http://7vzmkg.com1.z0.glb.clouddn.com/wave/7BEBA170DD7D22A4.mp3";
+    //    }
+    [self.traditionalWormhole passMessageObject:@{@"selectionString" : title} identifier:@"selection"];
+    [self.watchConnectivityWormhole passMessageObject:@{@"selectionString" : title} identifier:@"selection"];
+}
+
 
 @end
--- watchos Extension/InterfaceController.m
+++ watchos Extension/InterfaceController.m
@@ -9,7 +9,18 @@
 #import "InterfaceController.h"
 
 
-@interface InterfaceController()
+#import "MMWormhole.h"
+#import "MMWormholeSession.h"
+
+@interface InterfaceController()<NSURLSessionDelegate> {
+    NSURLSessionDataTask *_task;
+}
+
+@property (nonatomic, strong) MMWormhole *wormhole;
+@property (nonatomic, strong) MMWormholeSession *listeningWormhole;
+@property (nonatomic, strong) NSString* audioUrl;
+
+@property (nonatomic, weak) IBOutlet WKInterfaceLabel *selectionLabel;
 
 @end
 
@@ -18,18 +29,113 @@
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
-
-    // Configure interface objects here.
+    
+    // You are required to initialize the shared listening wormhole before creating a
+    // WatchConnectivity session transiting wormhole, as we are below.
+    self.listeningWormhole = [MMWormholeSession sharedListeningSession];
+    
+    // Initialize the wormhole
+    self.wormhole = [[MMWormhole alloc] initWithApplicationGroupIdentifier:@"group.com.lilkr.lilkrtest"
+                                                         optionalDirectory:@"wormhole"
+                                                            transitingType:MMWormholeTransitingTypeSessionContext];
+    
+    // Obtain an initial value for the selection message from the wormhole
+    id messageObject = [self.wormhole messageWithIdentifier:@"selection"];
+    NSString *string = [messageObject valueForKey:@"selectionString"];
+    
+    if (string != nil) {
+        [self.selectionLabel setText:string];
+    }
+    
+    // Listen for changes to the selection message. The selection message contains a string value
+    // identified by the selectionString key. Note that the type of the key is included in the
+    // name of the key.
+    [self.listeningWormhole listenForMessageWithIdentifier:@"selection" listener:^(id messageObject) {
+        NSString *string = [messageObject valueForKey:@"selectionString"];
+        
+        if (string != nil) {
+            [self.selectionLabel setText:string];
+        }
+        [self saveFileURL:string];
+    }];
+    
+    // Make sure we are activating the listening wormhole so that it will receive new messages from
+    // the WatchConnectivity framework.
+    [self.listeningWormhole activateSessionListening];
 }
+
 
 - (void)willActivate {
-    // This method is called when watch view controller is about to be visible to user
-    [super willActivate];
+    if (!_audioUrl) {
+        return;
+    }
+    if ([[self currentAudioURL] isEqualToString:_audioUrl]) {
+        [self playBtnTapped];
+    } else {
+        
+        [self saveFileURL:_audioUrl];
+    }
 }
 
-- (void)didDeactivate {
-    // This method is called when watch view controller is no longer visible
-    [super didDeactivate];
+
+- (void)saveFileURL:(NSString *)url {
+    
+    _audioUrl = [self currentAudioURL];
+    NSFileManager *defaultManager = [NSFileManager defaultManager];
+    
+    NSURL *saveUrl = [self currentSaveAudioURL];
+    
+    NSLog(@".................  %@", saveUrl.absoluteString);
+    
+    if ([defaultManager fileExistsAtPath:saveUrl.absoluteString]) {
+        [defaultManager removeItemAtURL:saveUrl error:nil];
+    }
+    
+    NSURLSessionConfiguration* configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
+    
+    _task = [session dataTaskWithURL:[NSURL URLWithString:url] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"xxxxxx...");
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [data writeToURL:saveUrl atomically:YES];
+        });
+        [self playBtnTapped];
+    }];
+    [_task resume];
+    
+}
+
+
+- (NSString *)currentAudioURL {
+    // Initialize the wormhole
+    self.wormhole = [[MMWormhole alloc] initWithApplicationGroupIdentifier:@"group.com.lilkr.lilkrtest"
+                                                         optionalDirectory:@"wormhole"
+                                                            transitingType:MMWormholeTransitingTypeSessionContext];
+    
+    // Obtain an initial value for the selection message from the wormhole
+    id messageObject = [self.wormhole messageWithIdentifier:@"selection"];
+    return [messageObject valueForKey:@"selectionString"];
+}
+
+
+- (NSURL *)currentSaveAudioURL {
+    return [[[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.com.lilkr.lilkrtest"]
+            URLByAppendingPathComponent:@"aaa.mp3"];
+}
+
+
+- (IBAction)playBtnPressed:(id)sender {
+    [self playBtnTapped];
+}
+
+- (void)playBtnTapped {
+    [self presentMediaPlayerControllerWithURL:[self currentSaveAudioURL] options:nil completion:^(BOOL didPlayToEnd, NSTimeInterval endTime, NSError * _Nullable error) {
+        
+    }];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(3);
+        [self dismissMediaPlayerController];
+    });
 }
 
 @end
